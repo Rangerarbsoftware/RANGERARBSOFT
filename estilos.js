96e63b81ae23f2bcc3405d9c1d386933b824b0f5
Rangerarbsoftware <rangerarbsoftwarelab@gmail.com>
2024-03-08 14:28:12 -0300

    Agregar constante lupa

--- estilos.js
+++ estilos.js
@@ -1,2 +1,4 @@
 //-- Mostrar y ocultar ventana de enlaces-menu --// 	
 const logo=document.querySelector('.logo');const enlaces=document.querySelector('.enlaces-menu');logo.addEventListener('click',() => {enlaces.classList.toggle('activado');}); 	
+//-- Mostrar/ocultar buscar(placeholder) --//
+const lupa=document.querySelector('.lupa');const muestraPlaceholder=document.querySelector('.icono-placeholder-image');lupa.addEventListener('click',() => {muestraPlaceholder.classList.toggle('activado');});
