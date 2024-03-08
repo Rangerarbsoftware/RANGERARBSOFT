ec1df418b5016a8ce310ae9d3e651fbcbfdc7fa8
Rangerarbsoftware <rangerarbsoftwarelab@gmail.com>
2024-03-08 14:02:40 -0300

    Imgresar la constante logo

---
+++ estilos.js
@@ -0,0 +1,2 @@
+//-- Mostrar y ocultar ventana de enlaces-menu --// 	
+const logo=document.querySelector('.logo');const enlaces=document.querySelector('.enlaces-menu');logo.addEventListener('click',() => {enlaces.classList.toggle('activado');}); 	
