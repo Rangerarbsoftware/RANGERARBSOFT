aaf5c696161f0471949c72d375df13ad15c7c6b6
Rangerarbsoftware <rangerarbsoftwarelab@gmail.com>
2024-03-22 11:05:27 -0300

    Configurar y encriptar correo y contraseña php

---
+++ configuraciones/usuarios/formvalidar.php
@@ -0,0 +1,31 @@
+<?php
+if($_SERVER["REQUEST_METHOD"]=="POST"){
+    include("conexion.php");
+
+    $email=(isset($_POST['email']))?htmlspecialchars($_POST['email']):null;
+    $password=(isset($_POST['password']))?htmlspecialchars($_POST['password']):null;
+
+    try{
+        $pdo=new PDO('mysql=host='.$direccionservidor.' ;dbname='.$baseDatos, $usuarioDB, $contrasenaDB);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql='SELECT * FROM registrousuarios WHERE email=:email';
+        $sentencia= $pdo->prepare($sql);
+        $sentencia->execute(['email'=>$email]);
+
+        $usuarios=$sentencia->fetch(PDO::FETCH_ASSOC);
+        $formvalidar=false;
+
+        foreach($usuarios as $user){
+            if(password_verify($password, $user['password'])){
+            //$_SESSION['loggedUser']=$user;
+            $formvalidar=true;
+            }
+        }
+
+    }catch(PDOException $e){
+
+    }
+
+}
+?>
