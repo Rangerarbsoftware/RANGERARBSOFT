f2b19daa15b5012514fe3ce9342b8850831e1bcb
Rangerarbsoftware <rangerarbsoftwarelab@gmail.com>
2024-03-22 16:39:36 -0300

    Validar equivalencia de password y reit_password js

--- estilos.js
+++ estilos.js
@@ -519,6 +519,13 @@ const formValidar = document.querySelector(".form-validar");
 validarPass.addEventListener("click", () => {
   formValidar.classList.toggle("activado");
 });
+var password = document.getElementById('password');
+var confirmarPassword = document.getElementById('password-reit');
+if(password !==password-reit){
+  document.getElementById('password-reit').classList.add('is-invalid');
+}else{
+  document.getElementById('password-reit').classList.remove('is-invalid');
+}
 //-- Mostrar Formulario de Recuperación --//
 const enviarCode = document.querySelector("#validar-pass");
 const formEnvCod = document.querySelector(".form-enviar-codigo");
